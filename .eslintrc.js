6db677be38db8eefec6b1fe8935af8afc1ff645d
rajkumar.arisetty <rajkumararisetty1993@gmail.com>
2018-07-01 12:42:21 +0530

    Added some eslint indent rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "indent": ["error", 2, {
+      "SwitchCase": 1,
+      "VariableDeclarator": {"var": 2, "let": 2, "const": 3},
+      "MemberExpression": 1,
+      "FunctionDeclaration": {"body": 1, "parameters": 2},
+      "CallExpression": {"arguments": "first"},
+      "ArrayExpression": "first",
+      "ObjectExpression": "first",
+      "ImportDeclaration": 1,
+      "flatTernaryExpressions": false
+    }]
   }
 };
